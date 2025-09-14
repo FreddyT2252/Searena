c0b182db99b4bbbb02e893b4430b1001ebf99b81
FreddyT2252 <79196216+FreddyT2252@users.noreply.github.com>
2025-09-14 15:12:43 +0700

    Update Destination.cs

--- src/SearenaApp/Destination.cs
+++ src/SearenaApp/Destination.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace SearenaApp
 {
-    internal class Destination
+    public class Destination
     {
+        private int destinationId;
+        private string name;
+        private string location;
+        private string description;
+        private string image;
+        public int DestinationId
+        {
+            get { return destinationId; }
+            set { destinationId = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        public string Image
+        {
+            get { return image; }
+            set { image = value; }
+        }
+        public void AddDestination()
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateDestination()
+        {
+            throw new NotImplementedException();
+        }
+        public void DeleteDestination()
+        {
+            throw new NotImplementedException();
+        }
+        public Destination ViewDestination()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
